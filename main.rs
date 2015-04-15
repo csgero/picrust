7bb3c09035579b83d8836bc829cfad3d9b3a8210
Csaba Gero <csgero@gmail.com>
2015-04-15 19:46:19 +0200

    Use pointer.offset for pointer arithmetics.

--- main.rs
+++ main.rs
@@ -1,18 +1,30 @@
 #![feature(lang_items)]
+#![feature(globs)]
 #![no_std]
+ 
+extern crate core;
+use core::prelude::*;
+
+#[no_mangle]
+pub extern fn main() {
+	let gpio_addr = 0x20200000 as *mut u32;
+	
+	unsafe {
+		*(gpio_addr.offset(1)) = 1 << 18;
+		*(gpio_addr.offset(10)) = 1 << 16;
+	}
+
+	loop {}
+}
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
 
-#[no_mangle]
-pub extern fn main() {
-	let gpio_addr: u32 = 0x20200000;
-	
-	unsafe {
-		*((gpio_addr + 4) as *mut u32) = 1 << 18;
-		*((gpio_addr + 40) as *mut u32) = 1 << 16;
-	}
-
-	loop {}
+//required for core	
+#[lang = "begin_unwind"]
+extern fn begin_unwind(args: &core::fmt::Arguments,
+                       file: &str,
+                       line: uint) -> ! {
+    loop {}
 }
