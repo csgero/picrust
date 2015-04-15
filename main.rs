1c959705c411672c697abc1073834b44c9a62975
Csaba Gero <csgero@gmail.com>
2015-04-15 20:46:40 +0200

    Generalize setting gpio function.

--- main.rs
+++ main.rs
@@ -5,16 +5,20 @@
 extern crate core;
 use core::prelude::*;
 
+static GPIO_ADDR: *mut uint = 0x20200000 as *mut uint;
+
 #[no_mangle]
-pub extern fn main() {
-	let gpio_addr = 0x20200000 as *mut u32;
-	
+pub extern fn main() {	
 	unsafe {
-		*(gpio_addr.offset(1)) = 1 << 18;
-		*(gpio_addr.offset(10)) = 1 << 16;
+		set_gpio_function(16, 1);
+		*(GPIO_ADDR.offset(10)) = 1 << 16;
 	}
 
 	loop {}
+}
+
+unsafe fn set_gpio_function(pin: uint, function_code: uint) {
+	*(GPIO_ADDR.offset((pin / 10) as int)) = function_code << ((pin % 10) * 3)
 }
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
